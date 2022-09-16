d39e5274cfac818b76e21a7ed52163740daa7b06
chrissiku <chrissiku5@gmail.com>
2022-09-16 19:52:58 +0200

    Add method to `create` new aricle

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -10,6 +10,22 @@ module Api
                 article = Article.find(params[:id])
                 render json: {status: 'SUCCESS', message: 'Loaded articles', data: article}, status: :ok
             end
+
+            def create
+                article = Article.new(article_params)
+                if article.save
+                    render json: {status: 'SUCCESS', message: 'Saved article', data: article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Article not saved', data: article.errors}, 
+                    status: :unprocessable_entity
+                end
+            end
+
+            private
+
+            def article_params
+                params.permit(:title, :body)
+            end
         end
     end
 end
