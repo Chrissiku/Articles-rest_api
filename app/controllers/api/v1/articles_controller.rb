9720044bd95eb5a2d564353d2b5523e0320b406d
chrissiku <chrissiku5@gmail.com>
2022-09-16 19:44:24 +0200

    Add `show` method. To show a single article to the route

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -5,6 +5,11 @@ module Api
                 articles = Article.order('created_at DESC')
                 render json: {status: 'SUCCESS', message: 'Loaded articles', data: articles}, status: :ok
             end
+
+            def show
+                article = Article.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded articles', data: article}, status: :ok
+            end
         end
     end
 end
